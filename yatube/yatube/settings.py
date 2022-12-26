10be01bc80e77c23e49bf59ddeb4cb6bf2693de5
Anastasia Lunegova <anastasia.lunegova@gmail.com>
2022-12-26 20:51:12 +0300

    Added secret key to env

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,10 +1,12 @@
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-SECRET_KEY = '#sc#feej)eq$f)3d9k1@97a4f#4dpw87sc8aywq11k6=t4!6!-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
